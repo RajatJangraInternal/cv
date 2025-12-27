befeeb43690f8886f4c25836dee61bc3612084ba
RajatJangra2653 <rajatjangra2653@gmail.com>
2025-12-27 15:44:48 +0530

    up

--- terraform/main.tf
+++ terraform/main.tf
@@ -117,6 +117,27 @@ resource "aws_lb_listener" "cv_listener" {
   }
 }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+
+# --- ALB and related resources (moved to top level) ---
+resource "aws_security_group" "alb_sg" {
+  name        = "cv-alb-sg"
+  description = "Allow HTTP access to ALB"
+  vpc_id      = aws_vpc.cv_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -124,6 +145,52 @@ resource "aws_lb_listener" "cv_listener" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  tags = {
+    Name = "cv-alb-sg"
+  }
+}
+
+resource "aws_lb" "cv_alb" {
+  name               = "cv-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.cv_subnet.id]
+  enable_deletion_protection = false
+  tags = {
+    Name = "cv-alb"
+  }
+}
+
+resource "aws_lb_target_group" "cv_tg" {
+  name     = "cv-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.cv_vpc.id
+  target_type = "ip"
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200-399"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+  tags = {
+    Name = "cv-tg"
+  }
+}
+
+resource "aws_lb_listener" "cv_listener" {
+  load_balancer_arn = aws_lb.cv_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.cv_tg.arn
+  }
+}
   tags = {
     Name = "cv-sg"
   }
